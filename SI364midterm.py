b1bf95527ff0fa89d2923e4e60c88475bbd65643
Luke Soenen <lukesoenen@gmail.com>
2018-11-04 09:44:24 -0600

    data

--- SI364midterm.py
+++ SI364midterm.py
@@ -5,45 +5,47 @@
 ## Import statements
 # Import statements
 import os
+import json, requests
 from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField# Note that you may need to import more here! Check out examples that do what you want to figure out what.
 from wtforms.validators import Required, Length # Here, too
 from flask_sqlalchemy import SQLAlchemy
-
 ## App setup code
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'this string will not be guessed'
-app.debug = True
-
 ## All app.config values
-
-
-## Statements for db setup (and manager setup if using Manager)
+app.config['SECRET_KEY'] = 'this string will not be guessed'
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/lsoenenMidterm"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+## Statements for db setup (and manager setup if using Manager)
 db = SQLAlchemy(app)
 ######################################
 ######## HELPER FXNS (If any) ########
 ######################################
+api_key = "m4zcqjsjr8e28jhhadwpcdvk"
 
-api_key = "43578d93330745dab735f5b49f76091c"
-api_secret = "859ad351a2b9457186dba6ab13973975"
 
 
 ##################
 ##### MODELS #####
 ##################
 
-class Name(db.Model):
-    __tablename__ = "names"
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(64))
+class Player(db.Model):
+    __tablename__ = "players"
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    position = db.Column(db.String)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"))
 
-    def __repr__(self):
-        return "{} (ID: {})".format(self.name, self.id)
+
+class Team(db.Model):
+    __tablename__ = "teams"
+    id = db.Column(db.Integer, primary_key = True)
+    school_name = db.Column(db.String)
+    school_mascot = db.Column(db.String)
+    players = db.relationship('Player', backref='Player')
 
 
 
@@ -51,31 +53,39 @@ class Name(db.Model):
 ###### FORMS ######
 ###################
 
-class NameForm(FlaskForm):
-    name = StringField("Please enter your name.",validators=[Required()])
-    submit = SubmitField("Submit")
+class TeamRosterForm(FlaskForm):
+    school_name = StringField('Enter the name of the school you would like to see a roster for:', validators=[Required()])
+    submit = SubmitField('Submit')
 
+class MascotForm(FlaskForm):
+    school_mascot = StringField('Enter the mascot to see a list of all schools with that mascot:', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 #######################
 ###### VIEW FXNS ######
 #######################
 
-@app.route('/')
-def home():
-    form = NameForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with GET
-    if form.validate_on_submit():
-        name = form.name.data
-        newname = Name(name)
-        db.session.add(newname)
-        db.session.commit()
-        return redirect(url_for('all_names'))
-    return render_template('base.html',form=form)
 
-@app.route('/names', methods=['GET'])
-def all_names():
-    names = Name.query.all()
-    return render_template('name_example.html',names=names)
+@app.route('/teamrosterform')
+def teamrosterform():
+    form = TeamRosterForm()
+    return render_template('teamrosterform.html', form=form)
+
+@app.route('/teamrosterinfo', methods=['GET','POST'])
+def teamrosterinfo():
+    form = TeamRosterForm()
+    school_name = form.school_name.data
+    base_url = "http://api.sportradar.us/ncaafb-t1/teams/" + str(school_name) + "/roster.json?api_key=" + api_key
+    response = requests.get(base_url)
+    text = response.text
+    python_obj = json.loads(text)
+    objects = python_obj
+    return render_template('teamrosterinfo.html', objects=objects)
+
+
+
+
 
 
 
